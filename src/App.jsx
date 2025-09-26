9d2c0b5cbc5334cc1b99a116577fb2c6b8286b05
V-ESPINOZA <vrespinoza@cased.edu.pe>
2025-09-26 13:36:12 -0500

    agregar json

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
+import   { useState, useEffect } from "react";
+import data from "./data/data.json";
+
 
 function App() {
+
+/*    useEffect(() => {
+    console.log("Profesores:", data.profesores);
+    console.log("Cursos:", data.cursos);
+  }, []); */
+
+/*   useEffect(() => {
+  console.log("El componente se renderizó 🔄");
+
   
+}); */
+
+  const [contador, setContador] = useState(0);
+
+  useEffect(() => {
+    console.log("El contador cambió:", contador);
+    console.log("Profesores:", data.profesores);
+  }, [contador]); 
+
 
   return (
+
     <>
-     
-      <h1>Vite + React</h1>
+    
+    <div>
+      <p>Contador1: {contador}</p>
+      <button onClick={() => setContador(contador + 1)}>Sumar</button>
+    </div>
      
      
     </>
